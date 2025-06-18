7fe15bea9c29699a9a00d473e2af78c82f1f9080
HubertLuszkiewicz <hluszkiewicz@gmail.com>
2025-06-18 12:29:31 +0200

    Add training and evaluation page

--- app.py
+++ app.py
@@ -1,13 +1,49 @@
 from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
+MODEL_SAVE_DIR = 'trained_models'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/train', methods=["GET"])
+def show_training_page():
+    return render_template('train_model.html')
+
+@app.route('/train', methods=["POST"])
+def train_model():
+    training_files = request.files.getlist('trainingData')
+
+    if not training_files:
+        return jsonify({"error": "No training data files received."}), 400
+
+    # training logic
+
+    return jsonify({
+        "status": "Data received",
+        "message": f"Successfully received {len(training_files)} training files. Processing...",
+        "training_file_count": len(training_files)
+    })
+
+@app.rout('/evaluate', methods=["GET"])
+def show_evaluation_page():
+    return render_template('evaluate_model.html')
+
+@app.route('/list_models', methods=['GET'])
+def list_available_models():
+    models_list = []
+    if os.path.exists(MODEL_SAVE_DIR):
+        for item_name in os.listdir(MODEL_SAVE_DIR):
+            item_path = os.path.join(MODEL_SAVE_DIR, item_name)
+            
+            models_list.append({"name": item_name, "path": item_path})
+
+    return jsonify(models_list)
+
 @app.route("/classify", methods=["GET"])
-def classify_image():
+def show_classification_page():
     return render_template('classify_image.html')
 
 @app.route("/classify", methods=["POST"])
@@ -24,6 +60,8 @@ def get_prediction():
         response = {"error": "No selected file."}
         return jsonify(response), 400
     
+    # Prediction logic
+
     # Successfull prediction
     predicted_label = "simulated_prediction" # Replace with actual
     confidence = 0.99 # Replace with actual
